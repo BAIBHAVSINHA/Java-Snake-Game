11ee75dcdeecd5ed9bc56eb764ddc1b3a5973142
Baibhav Sinha <60212736+BAIBHAVSINHA@users.noreply.github.com>
2020-09-19 19:05:05 +0530

    Update Board.java

--- Board.java
+++ Board.java
@@ -16,3 +16,10 @@ import javax.swing.JPanel;
 import javax.swing.Timer;
 
 public class Board extends JPanel implements ActionListener {
+
+  private final int B_WIDTH = 300;
+    private final int B_HEIGHT = 300;
+    private final int DOT_SIZE = 10;
+    private final int ALL_DOTS = 900;
+    private final int RAND_POS = 29;
+    private final int DELAY = 140;
