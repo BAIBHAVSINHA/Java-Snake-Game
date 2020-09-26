75f39a6275142de324a6b3f36c0e093525a183fb
Baibhav Sinha <60212736+BAIBHAVSINHA@users.noreply.github.com>
2020-09-26 19:25:28 +0530

    Update Board.java

--- Board.java
+++ Board.java
@@ -210,5 +210,23 @@ private void doDrawing(Graphics g)
 
         repaint();
     }
-    
+    private class TAdapter extends KeyAdapter {
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+            int key = e.getKeyCode();
+
+            if ((key == KeyEvent.VK_LEFT) && (!rightDirection)) {
+                leftDirection = true;
+                upDirection = false;
+                downDirection = false;
+            }
+
+            if ((key == KeyEvent.VK_RIGHT) && (!leftDirection)) {
+                rightDirection = true;
+                upDirection = false;
+                downDirection = false;
+            }
+            
 }
