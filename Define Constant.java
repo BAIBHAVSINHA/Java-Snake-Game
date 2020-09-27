49c7e931a921e2b80d147dc6161576cb069f0623
Baibhav Sinha <60212736+BAIBHAVSINHA@users.noreply.github.com>
2020-09-27 21:12:28 +0530

    Update Define Constant.java

--- Define Constant.java
+++ Define Constant.java
@@ -4,3 +4,8 @@ private final int DOT_SIZE = 10;
 private final int ALL_DOTS = 900;
 private final int RAND_POS = 29;
 private final int DELAY = 140;
+//(The B_WIDTH and B_HEIGHT constants determine the size of the board.
+// The DOT_SIZE is the size of the apple and the dot of the snake.
+//The ALL_DOTS constant defines the maximum number of possible dots on the board (900 = (300*300)/(10*10)).
+// The RAND_POS constant is used to calculate a random position for an apple.
+// The DELAY constant determines the speed of the game.)
