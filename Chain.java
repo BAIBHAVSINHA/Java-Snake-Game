381c070cf8bc3c25bd1af44932faf4ce764e944c
Baibhav Sinha <60212736+BAIBHAVSINHA@users.noreply.github.com>
2020-09-28 19:23:24 +0530

    Update Chain.java

--- Chain.java
+++ Chain.java
@@ -1,1 +1,24 @@
+for (int z = dots; z > 0; z--) {
+    x[z] = x[(z - 1)];
+    y[z] = y[(z - 1)];
+}
 
+//This code moves the joints up the chain.
+
+if (leftDirection) {
+    x[0] -= DOT_SIZE;
+}
+
+//In the checkCollision() method, we determine if the snake has hit itself or one of the walls.
+
+for (int z = dots; z > 0; z--) {
+
+    if ((z > 4) && (x[0] == x[z]) && (y[0] == y[z])) {
+        inGame = false;
+    }
+}
+//If the snake hits one of its joints with its head the game is over.
+
+if (y[0] >= B_HEIGHT) {
+    inGame = false;
+}
